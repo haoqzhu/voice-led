3c460ae1dd65a7fc6d65970363656425bc3cefa1
haoqinzh <haoqinzh@buffalo.edu>
2024-12-01 16:43:59 -0500

    new code

--- 321project.ino
+++ 321project.ino
@@ -1,65 +1,51 @@
 #include <SoftwareSerial.h>
-
-SoftwareSerial BTSerial(2, 3); //Red on 2, Black on 3
-
-#define LEDR 13
-#define LEDG 12
-#define LEDB 11
+SoftwareSerial BT(2, 3);
+char val; //value to receieve data from the serial port
+int LEDR = 13;
+int LEDG = 12;
+int LEDB = 11;
 #define analog_input A0
-#define digital_input A1
 
-void setup() {                
-  pinMode(LEDR, OUTPUT);
+void setup() {
+  pinMode(LEDR, OUTPUT); // set LED pins as outputs
   pinMode(LEDG, OUTPUT);
   pinMode(LEDB, OUTPUT);
   pinMode(analog_input, INPUT);
-  pinMode(digital_input, INPUT);
   Serial.begin(9600);
-  BTSerial.begin(9600);
-  Serial.print("Ready to accept commands");
+  BT.begin(9600); //begin serial communications at 9600 bits per second (baud)
 }
 
-int r = 0;
-int g = 0;
-int b = 0;
-
 void loop() {
-
-  if (Serial.available() > 0)
-    BTSerial.write(Serial.read());
-  if (BTSerial.available() > 0)
-    Serial.write(BTSerial.read());
-
   int analog_value = analogRead(analog_input); 
-  int digital_value = digitalRead(digital_input);
 
-  Serial.print(analog_value);
-
-  // Convert the analog value to voltage for display
-  float voltage = analog_value * (5.0 / 1023.0);
-    
-  Serial.print("Analog voltage value: "); 
-  Serial.println(voltage);
-  
-  Serial.println("----------------------------------------------------------------");
-
-  // Change LED color based on sound level
-  if (analog_value < 570) {
-    // Low sound level - Off
-    analogWrite(LEDR, 0);
-    analogWrite(LEDG, 0);
-    analogWrite(LEDB, 0);
-  } else if (analog_value < 800) {
-    // Medium sound level - Green
-    analogWrite(LEDR, 0);
-    analogWrite(LEDG, 255);
-    analogWrite(LEDB, 0);
-  } else {
-    // High sound level - Red
-    analogWrite(LEDR, 255);
-    analogWrite(LEDG, 0);
-    analogWrite(LEDB, 0);
+  if(BT.available()) { // check if there is data to read
+    val = BT.read(); // read the data into our value variable
+    BT.print("Reading new data: ");
+    BT.println(val);
   }
 
+  if (val == '1') { // if 1 (high) is received
+    if (analog_value < 570) {
+      // Low sound level - Off
+      analogWrite(LEDR, 0);
+      analogWrite(LEDG, 0);
+      analogWrite(LEDB, 0);
+    } else if (analog_value < 800) {
+      // Medium sound level - Green
+      analogWrite(LEDR, 0);
+      analogWrite(LEDG, 255);
+      analogWrite(LEDB, 0);
+    } else {
+      // High sound level - Red
+      analogWrite(LEDR, 255);
+      analogWrite(LEDG, 0);
+      analogWrite(LEDB, 0);
+    }
+  } else { // 1 not received
+    digitalWrite(LEDR, LOW); // Turn on the red light and off the green light
+    digitalWrite(LEDG, LOW);
+    digitalWrite(LEDB, HIGH);
+  }
+  
   delay(100);
 }
